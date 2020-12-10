97888533171a48f25e7ed214c60658ddc475b892
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-10 22:29:13 +0200

    functions example commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  //function
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+void buildKey(){
+  Expanded(
+    child: FlatButton(
+        color: Colors.red,
+        onPressed: () {
+          playSound(1);
+        },
+        child: Text('Do')),
+  );
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text('Click me'),
-          )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey();
+              buildKey();
+              buildKey();
+              buildKey();
+              buildKey();
+              buildKey();
+              buildKey();
+            ],
+          ),
         ),
       ),
     );
